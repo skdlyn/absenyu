121e06fc7255b52aab6d2d1b1b5c7b928acd45ac
skdlyn <rafli.dwiferdiansyah01@gmail.com>
2022-11-02 18:08:39 +0700

    sidebar

--- resources/views/layout/sidebar.blade.php
+++ resources/views/layout/sidebar.blade.php
@@ -38,7 +38,7 @@
         <div id="collapseUtilities" class="collapse" aria-labelledby="headingUtilities"
             data-parent="#accordionSidebar">
             <div class="bg-white py-2 collapse-inner rounded">
-                <h6 class="collapse-header">Pilihan :</h6>
+                <h6 class="collapse-header">Pilihan Menu :</h6>
                 <a class="collapse-item" href="/datasiswa">Data Siswa</a>
                 <a class="collapse-item" href="/datakelas">Data Kelas</a>
                 <a class="collapse-item" href="/guru">Data Guru</a>
