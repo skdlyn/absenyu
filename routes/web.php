42225084ad2fddeca33b860e50e8333994185684
ilhmstar <ilhambintang30332@gmail.com>
2023-01-18 15:05:30 +0700

    otw merge

--- routes/web.php
+++ routes/web.php
@@ -9,7 +9,6 @@ use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\KelasController;
 use App\Http\Controllers\DataKelasController;
 use App\Http\Controllers\AbsenController;
-use App\Http\Controllers\CitiesController;
 use App\Http\Controllers\SiswaController;
 use App\Http\Controllers\DataSiswaController;
 use App\Http\Controllers\DaftarAbsenController;
@@ -56,30 +55,10 @@ Route::middleware('auth')->group(function () {
     Route::resource('absen', AbsenController::class);
     route::get('absen/{absen}', [AbsenController::class, 'absen'])->name('absen.tambah');
     route::get('absen/{absen}/listabsen', [AbsenController::class, 'listabsen'])->name('absen.list');
-    // route::get('list', [AbsenController::class,'list'])->name('list.absen');
-    // route::get('absen/create',[AbsenController::class, 'create'])->name('absenc.create');
-    // route::get('absen/{id}/')
-    // Route::get('list', [AbsenController::class, 'list'])->name('absen.list');
-    // Route::get('listkelas', [AbsenController::class, 'listkelas'])->name('absen.listkelas');
-    // Route::get('pending', [AbsenController::class, 'pending'])->name('absen.pending');
-    // route::get('/listabsen', function(){
-    //     return view('absen.listabsen');
-    // });
-
-
-    //  absen
-    // route::get('listabsen',function(){
-    //     return view('absen.listabsen');
-    // });
-    //  route::resource('lisabsen', 'ListAbsenController');
-
 
 
 
     // CRUD DATA KELAS
-    // Route::resource('datasiswa', DataController::class);
-    // Route::get('datasiswa/{id}', [DataController::class, 'hapus'])->name('datasiswa.hapus');
-
 
     // CRUD KELAS
     Route::resource('datakelas', DataKelasController::class);
@@ -87,9 +66,6 @@ Route::middleware('auth')->group(function () {
 
     // CRUD SISWA
     Route::resource('datasiswa', DatasiswaController::class);
-    // route::get('/tambahsiswa',function(){
-    //     return view('kelas.tambahsiswa');
-    // });
     route::get('datakelas/{id}/hapus', [DatasiswaController::class, 'hapus'])->name('datasiswa.hapus');
 
 
@@ -97,24 +73,7 @@ Route::middleware('auth')->group(function () {
 
     Route::resource('guru', GuruController::class);
     Route::get('guru/{nama}/hapus', [GuruController::class, 'hapus'])->name('guru.hapus');
-
-    // route::get('/rekaplist', function(){
-    //     return view('rekaplist');
-    // });
-
-
-
     Route::post('logout', [LoginController::class, 'logout']);
-    // cobaroute
-
-    // route::get('/showkelas',[DataKelasController::class]);
-    // route::get('/showkelas', function(){
-    //     return view('showkelas');
-    // });
-
-    // coba nested controller
-    // Route::resource('countries.cities', CitiesController::class);
-
     route::resource('country.cities', CitiesController::class);
 
     route::get('list', [DaftarAbsenController::class, 'list'])->name('kelas.tanggal.daftar');
