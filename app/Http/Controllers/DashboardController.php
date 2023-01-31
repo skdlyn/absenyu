3e1f2c120228576fd9e35b44b48129a50baf2362
ilhmstar <ilhambintang30332@gmail.com>
2023-01-31 13:18:39 +0700

    membenarkan autentikasi 1

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -10,12 +10,25 @@ use Illuminate\Http\Request;
 
 class dashboardcontroller extends Controller
 {
-    public function dash(){
-        
+    // public function __construct()
+    // {
+    //  
+    // }
+
+    public function dash()
+    {
         $jumlah = guru::all()->count();
         $jumlah2 = siswa::all()->count();
         $jumlah3 = kelas::all()->count();
-        // return ($jumlah);
-        return view('dashboard', compact('jumlah', 'jumlah2', 'jumlah3'));
+
+        if (auth()->user()->role =='admin') {
+            return view('dashboard', compact('jumlah', 'jumlah2', 'jumlah3'));
+        }else if(auth()->user()->role =='sekertaris'){
+            return 'sekre';
+        }else if(auth()->user()->role =='bk'){
+            return 'bk';
+        }else{
+            return view('dashboard',compact('jumlah', 'jumlah2', 'jumlah3'));
+        }
     }
 }
